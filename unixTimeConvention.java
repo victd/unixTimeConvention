24cd660265cbfcaab8781e4fdeccdd4461eb7396
victd <55512729+victd@users.noreply.github.com>
2022-02-07 16:13:09 -0500

    Add files via upload

---
+++ unixTimeConvention.java
@@ -0,0 +1,8 @@
+package com.example.vicsoundpool;
+import java.util.Date, java.util.Calendar;
+
+public class unixTimeConvention {
+    Date date = new Date();
+    long unixTime = date.getTime() / 1000L;
+// 1577094646
+}
