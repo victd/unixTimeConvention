8e55ae63f8f80c2bf916b2b4d5060c43a9ec2a7b
victd <55512729+victd@users.noreply.github.com>
2022-02-08 10:06:22 -0500

    Update unixTimeConvention.java

--- unixTimeConvention.java
+++ unixTimeConvention.java
@@ -4,5 +4,7 @@ import java.util.Date, java.util.Calendar;
 public class unixTimeConvention {
     Date date = new Date();
     long unixTime = date.getTime() / 1000L;
-// 1577094646
+// 1644332696
+// this is Tuesday, February 8, 2022 10:04:56 AM GMT-05:00
+// Eastern Standard Time is GMT minus 5:00
 }
