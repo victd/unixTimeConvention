76670513a01091e7eae92e6a53d21951777826c4
victd <55512729+victd@users.noreply.github.com>
2023-06-26 08:19:43 -0400

    Update unixTimeConvention.java

--- unixTimeConvention.java
+++ unixTimeConvention.java
@@ -7,6 +7,7 @@ public class unixTimeConvention {
 // 1644332696
 // this is Tuesday, February 8, 2022 10:04:56 AM GMT-05:00
 // Eastern Standard Time is GMT minus 5:00, project to stay on standard or savings time
+// accounts for daylight savings or standard across each region
 // conventional way of unix time conversion
     // incorporate this into the handclock Schlage timeclock
     // compare with python class
